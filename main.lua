a3b89aa9be60ec02bf45f747086b9ab974329ce6
UnDecrypted <155349824+UnDecrypted@users.noreply.github.com>
2024-05-28 21:02:57 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
-
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UnDecrypted/Berzerk-Hub/main/loader.lua"))()
 local Premium = loadstring(game:HttpGet("https://raw.githubusercontent.com/UnDecrypted/Berzerk-Hub/main/premium.lua"))()
+load(Premium)
