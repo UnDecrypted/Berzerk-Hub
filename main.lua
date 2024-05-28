632fbbde63ed16512db698286624664d62de91a7
UnDecrypted <155349824+UnDecrypted@users.noreply.github.com>
2024-05-28 21:01:14 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,21 +1,3 @@
-function load(premium)
-  local Game = {
-    {101010,"",""}
-  }
 
-  print(game.PlaceId)
-  
-  for i, v in pairs(Game) do
-    if game.PlaceId == v[1] then
-      if premium then
-        loadstring(game:HttpGet(v[3]))()
-        SendNotif("Success To Load Script : "..game.name)
-      else
-        loadstring(game:HttpGet(v[2]))()
-        SendNotif("Success To Load Script : "..game.name)
-      end
-      Loaded = true
-      return
-    end
-  end
-end
+
+local Premium = loadstring(game:HttpGet("https://raw.githubusercontent.com/UnDecrypted/Berzerk-Hub/main/premium.lua"))()
