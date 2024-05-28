0e9a2f40c23fda92d38e8d3ee9e6587e5e503147
UnDecrypted <155349824+UnDecrypted@users.noreply.github.com>
2024-05-28 21:00:34 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,21 @@
+function load(premium)
+  local Game = {
+    {101010,"",""}
+  }
 
+  print(game.PlaceId)
+  
+  for i, v in pairs(Game) do
+    if game.PlaceId == v[1] then
+      if premium then
+        loadstring(game:HttpGet(v[3]))()
+        SendNotif("Success To Load Script : "..game.name)
+      else
+        loadstring(game:HttpGet(v[2]))()
+        SendNotif("Success To Load Script : "..game.name)
+      end
+      Loaded = true
+      return
+    end
+  end
+end
