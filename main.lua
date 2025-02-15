1368c8be5b4d526ff88cfb26750aff55e74725db
UnDecrypted <155349824+UnDecrypted@users.noreply.github.com>
2025-02-15 11:27:26 +0700

    Delete main.lua

--- main.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UnDecrypted/Berzerk-Hub/main/loader.lua"))()
-local Premium = loadstring(game:HttpGet("https://raw.githubusercontent.com/UnDecrypted/Berzerk-Hub/main/premium.lua"))()
-load(Premium)
