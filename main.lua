37b94dcf0099def504c8e5d478d8ff688c17bf92
UnDecrypted <155349824+UnDecrypted@users.noreply.github.com>
2025-02-19 15:37:54 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -84,12 +84,13 @@ AddHook("OnRender", "Berzerk", function(DeltaTime)
           for i, v in pairs(scripts) do
               if ImGui.BeginTabItem(v.name) then
                 if CurTab ~= v.name then
-                    Func = load(makeRequest(v.link, "GET").content)()
+                    load(makeRequest(v.link, "GET").content)()
                     CurTab = v.name
                 end
-                if Func then
-                    Func()
+                if run then
+                    run()
                 end
+
                 ImGui.EndTabItem()
               end
           end
